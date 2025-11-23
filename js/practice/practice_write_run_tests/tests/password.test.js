2427c9ac1c6f90c885970c2af9a61fa0b0adbd91
Navid <nh222yr@student.lnu.se>
2025-11-23 14:38:07 +0100

    test: done with part 1

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -7,10 +7,10 @@
 // import { Password } from '../src/BugMissingNumberCheck'
 // import { Password } from '../src/BugMissingPasswordCheck'
 // import { Password } from '../src/BugNeverContainsNumbers'
-// import { Password } from '../src/BugToShortPassword'
-// import { Password } from '../src/BugVeryShort'
+//  import { Password } from '../src/BugToShortPassword'
+ // import { Password } from '../src/BugVeryShort'
 // import { Password } from '../src/BugWrongHashingAlgorithm'
-// import { Password } from '../src/BugWrongMessage'
+ //import { Password } from '../src/BugWrongMessage'
  import { Password } from '../src/Correct'
 
 describe('Password class, test suite', () => {
@@ -35,6 +35,28 @@ describe('Password class, test suite', () => {
    
     test('chek if number required', () => {
       
-      expect(() => new Password('mypasswordmvp')).toThrow('No number found')
+     expect(() => new Password('mypasswordmvp')).toThrow('No number found')
     })
- })
+    test('check missing password',() => {
+      
+      expect(() => new Password('mvpmypassworda')).toThrow('No number found')
+    })
+    })
+    test('check if conatine number',() => {
+        expect(() => new Password('mypasswordmvp1').no.toThrow())
+    })
+    test('check too short password',() => {
+      expect(() => new Password('mypassword1')).toThrow('Too short password')
+    })
+    test('check very short password',() => {
+      expect(() => new Password('123q').toThrow('Too short password'))
+    })
+   test('check if generates different hashes for different password ',() =>{
+    const pas1 = new Password('12345mvppassword')
+    const pas2 = new Password('54321mvppassword')
+    expect(pas1.getPasswordHash()).not.toBe(pas2.getPasswordHash())
+   })
+   test('ckeck wrong message',() => {
+    expect(() => new Password('short1')).toThrow('Too short password')
+   })
+
